85ef27631ed896e02fe6b4294e40d7eb40f4a177
sha-nan <17609569016@163.com>
2022-04-22 23:20:05 +0800

    Add helloword.c File

---
+++ helloword.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    printf("Hello World!\n");
+    //冒泡排序
+    int a[10] = {11,2,3,4,5,6,7,8,22,10};
+    int i,j,temp;
+    for(i=0;i<10;i++)
+    {
+        for(j=0;j<10-i;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                temp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = temp;
+            }
+        }
+    }
+    for(i=0;i<10;i++)
+    {
+        printf("%d ",a[i]);
+    }
+    return 0;
+}
